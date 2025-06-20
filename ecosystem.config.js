877a0b0acef9c11b7b013a3704269e2e7f8001bc
scriptsmay <scriptsmay@163.com>
2025-06-20 10:10:14 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,7 @@
+function getDelayTime(hour) {
+  return hour * 60 * 60 * 1000;
+}
+
 module.exports = {
   apps: [
     // 主应用 - Express服务器
@@ -24,19 +28,9 @@ module.exports = {
       cron_restart: '0 9 * * *', // 每天9点运行
       autorestart: false, // 必须设置为false才能使用cron
       watch: false,
-    },
-    {
-      name: 'huya-kpl-task',
-      script: './scripts/huya-kpl.js',
-      cron_restart: '1 0 * * *',
-      autorestart: false,
-    },
-    {
-      name: 'huya-live-auto',
-      script: './scripts/auto-huya.js',
-      cron_restart: '5 0 * * *',
-      autorestart: false,
-      watch: false,
+      // 添加以下配置避免首次执行
+      restart_delay: getDelayTime(1), // 1小时延迟(毫秒)
+      force: true,
     },
     {
       name: 'huya-live-badge',
@@ -44,6 +38,29 @@ module.exports = {
       cron_restart: '0 11 * * *',
       autorestart: false,
       watch: false,
+      // 添加以下配置避免首次执行
+      restart_delay: getDelayTime(2),
+      force: true,
+    },
+    {
+      name: 'huya-kpl-task',
+      script: './scripts/huya-kpl.js',
+      cron_restart: '1 0 * * *',
+      autorestart: false,
+      watch: false,
+      // 添加以下配置避免首次执行
+      restart_delay: getDelayTime(3),
+      force: true,
+    },
+    {
+      name: 'huya-live-auto',
+      script: './scripts/auto-huya.js',
+      cron_restart: '5 0 * * *',
+      autorestart: false,
+      watch: false,
+      // 添加以下配置避免首次执行
+      restart_delay: getDelayTime(4),
+      force: true,
     },
 
     // 定时任务2
