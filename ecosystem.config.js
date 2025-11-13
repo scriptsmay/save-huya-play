ed59ba648286c3ee71aa975f8a197a5f6fca77e4
scriptsmay <scriptsmay@163.com>
2025-11-13 17:35:08 +0800

    pm2 list

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,7 @@ module.exports = {
     // 主应用 - Express服务器
     {
       name: 'server',
-      script: 'npm',
-      args: 'start',
+      script: './app/index.js',
       watch: true, // 可选：监听文件变化自动重启
       ignore_watch: [
         // 可选：忽略监听的目录
@@ -19,24 +18,6 @@ module.exports = {
     },
 
     // 定时任务
-    // {
-    //   name: 'kpl-everyday',
-    //   script: './scripts/kpl-everyday.js',
-    //   cron_restart: '0 9 * * *', // 每天9点运行
-    //   autorestart: false, // 必须设置为false才能使用cron
-    //   watch: false,
-    // },
-    // 测试了一下，只有这个任务能正常运行，以下会报错
-    // 还是老老实实使用crontab吧
-    // {
-    //   name: 'huya-live-badge',
-    //   script: './scripts/huya-badgelist.js',
-    //   cron_restart: '0 11 * * *',
-    //   autorestart: false,
-    //   watch: false,
-    // },
-
-    // 定时任务2
     // 1点运行
     {
       name: 'hourly-cleanup',
